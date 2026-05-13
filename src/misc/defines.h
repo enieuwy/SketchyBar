706b684b8a2f731ea2e07cb332eda54a86efa8bb
enieuwy <ellis_125@hotmail.com>
2026-05-14 00:51:56 +0800

    feat: add badge anchors and ring marker

--- src/misc/defines.h
+++ src/misc/defines.h
@@ -44,6 +44,7 @@
 #define SUB_DOMAIN_SHADOW                      "shadow"
 #define SUB_DOMAIN_IMAGE                       "image"
 #define SUB_DOMAIN_BADGE                       "badge"
+#define SUB_DOMAIN_MARKER                      "marker"
 #define SUB_DOMAIN_KNOB                        "knob"
 #define SUB_DOMAIN_SLIDER                      "slider"
 #define SUB_DOMAIN_FONT                        "font"
